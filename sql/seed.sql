42b448036d2166b9bd9bb82b591e4d368a6d48aa
CarlijnPelk <carlijnpelk@hotmail.com>
2020-12-10 08:42:45 +0000

    land_id toegevoegd

--- sql/seed.sql
+++ sql/seed.sql
@@ -5,53 +5,53 @@
 --
 
 
-insert into products (name, description, code, price) values ('Sushi', 'Rijstgerecht met zeewier en vis + gratis wasabi, sojasaus en stokjes.', 'japan-01', 3);
-insert into products (name, description, code, price) values ('Sushi box', 'Rijstgerecht met zeewier en vis + gratis wasabi, sojasaus en stokjes. Combinatie-box van 20 stuks', 'japan-02', 25);
-insert into products (name, description, code, price) values ('Noodles', 'Sliertjes met groentes en garnalen.', 'japan-03', 8);
-insert into products (name, description, code, price) values ('Temaki', 'Sushi, maar dan groot.', 'japan-04', 5);
-insert into products (name, description, code, price) values ('Mihoen Goreng', 'Dunne noodles die lekker smaken', 'japan-05', 9);
-insert into products (name, description, code, price) values ('Nasi Goreng', 'Rijst dat lekker smaakt', 'japan-06', 6);
-insert into products (name, description, code, price) values ('Edamame', 'Lekkere sojaboontjes die goed zijn voor je gezondheid', 'japan-07', 2);
-insert into products (name, description, code, price) values ('Yakitori', 'Malse kipspiesjes met zoetzure saus.', 'japan-08', 5);
-insert into products (name, description, code, price) values ('Tatsuta-age', 'Gefrituurde kip, gemarineerd in sojasaus en gember.', 'japan-09', 6);
-insert into products (name, description, code, price) values ('Okonomiyaki', 'Pannekoekjes met zeewier, groenten, vlees en vis.', 'japan-10', 13);
-insert into products (name, description, code, price) values ('Shabu shabu', 'Pannetje bouillon met vlees, vis, groenten, noodles en dim sum.', 'japan-11' 12);
-insert into products (name, description, code, price) values ('Sukiyaki', 'In bouillon gesmoorde groenten, wagyu, tofu, noodles, bieslook, kool en paddenstoeltjes.', 'japan-12', 14);
+insert into products (name, description, code, price, land_id) values ('Sushi', 'Rijstgerecht met zeewier en vis + gratis wasabi, sojasaus en stokjes.', 'japan-01', 3, 1);
+insert into products (name, description, code, price, land_id) values ('Sushi box', 'Rijstgerecht met zeewier en vis + gratis wasabi, sojasaus en stokjes. Combinatie-box van 20 stuks', 'japan-02', 25, 1);
+insert into products (name, description, code, price, land_id) values ('Noodles', 'Sliertjes met groentes en garnalen.', 'japan-03', 8), 1;
+insert into products (name, description, code, price, land_id) values ('Temaki', 'Sushi, maar dan groot.', 'japan-04', 5, 1);
+insert into products (name, description, code, price, land_id) values ('Mihoen Goreng', 'Dunne noodles die lekker smaken', 'japan-05', 9, 1);
+insert into products (name, description, code, price, land_id) values ('Nasi Goreng', 'Rijst dat lekker smaakt', 'japan-06', 6), 1;
+insert into products (name, description, code, price, land_id) values ('Edamame', 'Lekkere sojaboontjes die goed zijn voor je gezondheid', 'japan-07', 2, 1);
+insert into products (name, description, code, price, land_id) values ('Yakitori', 'Malse kipspiesjes met zoetzure saus.', 'japan-08', 5, 1);
+insert into products (name, description, code, price, land_id) values ('Tatsuta-age', 'Gefrituurde kip, gemarineerd in sojasaus en gember.', 'japan-09', 6, 1);
+insert into products (name, description, code, price, land_id) values ('Okonomiyaki', 'Pannekoekjes met zeewier, groenten, vlees en vis.', 'japan-10', 13, 1);
+insert into products (name, description, code, price, land_id) values ('Shabu shabu', 'Pannetje bouillon met vlees, vis, groenten, noodles en dim sum.', 'japan-11' 12, 1);
+insert into products (name, description, code, price, land_id) values ('Sukiyaki', 'In bouillon gesmoorde groenten, wagyu, tofu, noodles, bieslook, kool en paddenstoeltjes.', 'japan-12', 14, 1);
 
-insert into products (name, description, code, price) values ('Tagliatelle met arnalen', 'Romige verse tagliatelle met garnalen.', 'italië-01', 12.5);
-insert into products (name, description, code, price) values ('Lasagne', 'Een zomers gerecht met verse groentes en huisgemaakte bladen.', 'italië-02', 14);
-insert into products (name, description, code, price) values ('Risotto', 'Romig rijsgerecht met een rijk bospaddenstoel assortiment.', 'italië-03', 13.5);
-insert into products (name, description, code, price) values ('Carpaccio', 'Dungesneden rundercarpaccio met knapperige pijnboompitten, rucola en parmezaanse kaas.', 'italië-04', 5);
-insert into products (name, description, code, price) values ('Vitello tonnato', 'Een klassiek Italiaans zomergerecht met malse kalf en tonijn.', 'italië-05', 5.5);
-insert into products (name, description, code, price) values ('Gnocchi ovenschotel', 'Heerlijk in de oven gegaarde gnocchi met kip en spinazie.', 'italië-06', 14.5);
-insert into products (name, description, code, price) values ('Spaghetti aglio e olio', 'Typisch Italiaans voorgerecht met knoflook, olijfolie en peterselie.', 'italië-07', 8.5);
-insert into products (name, description, code, price) values ('Caprese salade', 'Fris voorgerecht met tomaat, mozarella en basilicum.', 'italië-08', 6.5);
-insert into products (name, description, code, price) values ('Gelato-asorti', 'Assortiment van verschillende ijsbolletjes.', 'italië-09', 8);
-insert into products (name, description, code, price) values ('Tiramisu', 'Lekker Italiaans toetje met eem scheutje alcohol.', 'italië-10', 7);
-insert into products (name, description, code, price) values ('Ravioli', 'Gevulde kussentjes met tomaatjes uit de oven', 'italië-11', 14.5);
-insert into products (name, description, code, price) values ('Bruschetta', 'Knapperig stokbrood met tomaat en ui.', 'italië-12', 4);
+insert into products (name, description, code, price, land_id) values ('Tagliatelle met arnalen', 'Romige verse tagliatelle met garnalen.', 'italië-01', 12.5, 2);
+insert into products (name, description, code, price, land_id) values ('Lasagne', 'Een zomers gerecht met verse groentes en huisgemaakte bladen.', 'italië-02', 14, 2);
+insert into products (name, description, code, price, land_id) values ('Risotto', 'Romig rijsgerecht met een rijk bospaddenstoel assortiment.', 'italië-03', 13.5, 2);
+insert into products (name, description, code, price, land_id) values ('Carpaccio', 'Dungesneden rundercarpaccio met knapperige pijnboompitten, rucola en parmezaanse kaas.', 'italië-04', 5, 2);
+insert into products (name, description, code, price, land_id) values ('Vitello tonnato', 'Een klassiek Italiaans zomergerecht met malse kalf en tonijn.', 'italië-05', 5.5, 2);
+insert into products (name, description, code, price, land_id) values ('Gnocchi ovenschotel', 'Heerlijk in de oven gegaarde gnocchi met kip en spinazie.', 'italië-06', 14.5, 2);
+insert into products (name, description, code, price, land_id) values ('Spaghetti aglio e olio', 'Typisch Italiaans voorgerecht met knoflook, olijfolie en peterselie.', 'italië-07', 8.5, 2);
+insert into products (name, description, code, price, land_id) values ('Caprese salade', 'Fris voorgerecht met tomaat, mozarella en basilicum.', 'italië-08', 6.5, 2);
+insert into products (name, description, code, price, land_id) values ('Gelato-asorti', 'Assortiment van verschillende ijsbolletjes.', 'italië-09', 8, 2);
+insert into products (name, description, code, price, land_id) values ('Tiramisu', 'Lekker Italiaans toetje met eem scheutje alcohol.', 'italië-10', 7, 2);
+insert into products (name, description, code, price, land_id) values ('Ravioli', 'Gevulde kussentjes met tomaatjes uit de oven', 'italië-11', 14.5, 2);
+insert into products (name, description, code, price, land_id) values ('Bruschetta', 'Knapperig stokbrood met tomaat en ui.', 'italië-12', 4, 2);
 
-insert into products (name, description, code, price) values ('Pannenkoeken', 'American pancakes met spek, boter, stroop en fruit.', 'amerika-01', 6);
-insert into products (name, description, code, price) values ('Hash browns', 'Rösti koekjes met ui en ei.', 'amerika-02', 10.5);
-insert into products (name, description, code, price) values ('Gevulde aardappels', 'Aardappels gevuld met bosui en bacon.', 'amerika-03', 14.5);
-insert into products (name, description, code, price) values ('Spare ribs', 'Lang gegaarde spare ribs met zoete saus', 'amerika-04', 14.5);
-insert into products (name, description, code, price) values ('Pizza Hawaii', 'Geen woorden voor, gratis af te halen.', 'amerika-05', 0);
-insert into products (name, description, code, price) values ('Hot dogs', 'Broodjes met worst en saus', 'amerika-06', 4);
-insert into products (name, description, code, price) values ('Kalkoen', 'Kalkoen', 'amerika-07', 10.5);
-insert into products (name, description, code, price) values ('BLT sandwich', 'Stokbrood met frisse sla, bacon en tomaat getopt met mayonaise.', 'amerika-08', 5);
-insert into products (name, description, code, price) values ("Mac 'n cheese", 'Macaroni met kaas kaas en nog eens kaas.', 'amerika-09', 8);
-insert into products (name, description, code, price) values ('Cupcakes', 'Klein, vrolijk cakeje.', 'amerika-10', 3.5);
-insert into products (name, description, code, price) values ('Bagel', 'Belegd broodje met gerookte zalm en bieslookroomkaas.', 'amerika-11', 7,5);
-insert into products (name, description, code, price) values ('Donut', 'Zoet broodje met glazuur topping', 'amerika-12', 4,5);
+insert into products (name, description, code, price, land_id) values ('Pannenkoeken', 'American pancakes met spek, boter, stroop en fruit.', 'amerika-01', 6, 3);
+insert into products (name, description, code, price, land_id) values ('Hash browns', 'Rösti koekjes met ui en ei.', 'amerika-02', 10.5, 3);
+insert into products (name, description, code, price, land_id) values ('Gevulde aardappels', 'Aardappels gevuld met bosui en bacon.', 'amerika-03', 14.5, 3);
+insert into products (name, description, code, price, land_id) values ('Spare ribs', 'Lang gegaarde spare ribs met zoete saus', 'amerika-04', 14.5, 3);
+insert into products (name, description, code, price, land_id) values ('Pizza Hawaii', 'Geen woorden voor, gratis af te halen.', 'amerika-05', 0, 3);
+insert into products (name, description, code, price, land_id) values ('Hot dogs', 'Broodjes met worst en saus', 'amerika-06', 4, 3);
+/*insert into products (name, description, code, price, land_id) values ('Hot dogs', 'Broodjes met worst en saus', 'amerika-07', 4, 3);*/
+insert into products (name, description, code, price, land_id) values ('BLT sandwich', 'Stokbrood met frisse sla, bacon en tomaat getopt met mayonaise.', 'amerika-08', 5, 3);
+insert into products (name, description, code, price, land_id) values ("Mac 'n cheese", 'Macaroni met kaas kaas en nog eens kaas.', 'amerika-09', 8, 3);
+insert into products (name, description, code, price, land_id) values ('Cupcakes', 'Klein, vrolijk cakeje.', 'amerika-10', 3.5, 3);
+insert into products (name, description, code, price, land_id) values ('Bagel', 'Belegd broodje met gerookte zalm en bieslookroomkaas.', 'amerika-11', 7,5, 3);
+insert into products (name, description, code, price, land_id) values ('Donut', 'Zoet broodje met glazuur topping', 'amerika-12', 4,5, 3);
 
 /*
-insert into products (name, description, code, price) values ('Crêpe Normade', 'Flinterdun pannekoekje', 'frankrijk-01', 3.5);
-insert into products (name, description, code, price) values ('Baguette', 'Stokbrood met kaas en tomaat', 'frankrijk-02', 2);
-insert into products (name, description, code, price) values ('Coquilles', 'Zachte zeevrucht, vers gevangen.', 'frankrijk-03', 21);
-insert into products (name, description, code, price) values ('Croissants', 'Luchtige broodjes met bourgondische jam.', 'frankrijk-04', 2);
-insert into products (name, description, code, price) values ('Crème Brûlée', 'Custard gebak met suiker laagje.', 'frankrijk-05', 6);
-insert into products (name, description, code, price) values ('Boeuf bourguignon', 'In rode wijn gestoofd rundvlees.', 'frankrijk-06', 16);
-insert into products (name, description, code, price) values ('Russendisko', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+insert into products (name, description, code, price, land_id) values ('Crêpe Normade', 'Flinterdun pannekoekje', 'frankrijk-01', 3.5);
+insert into products (name, description, code, price, land_id) values ('Baguette', 'Stokbrood met kaas en tomaat', 'frankrijk-02', 2);
+insert into products (name, description, code, price, land_id) values ('Coquilles', 'Zachte zeevrucht, vers gevangen.', 'frankrijk-03', 21);
+insert into products (name, description, code, price, land_id) values ('Croissants', 'Luchtige broodjes met bourgondische jam.', 'frankrijk-04', 2);
+insert into products (name, description, code, price, land_id) values ('Crème Brûlée', 'Custard gebak met suiker laagje.', 'frankrijk-05', 6);
+insert into products (name, description, code, price, land_id) values ('Boeuf bourguignon', 'In rode wijn gestoofd rundvlees.', 'frankrijk-06', 16);
+insert into products (name, description, code, price, land_id) values ('Russendisko', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
 
 Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
 
@@ -233,18 +233,5 @@ Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan fe
 insert into products (name, description, code, price) values ('The Four Feathers', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '704137368-1', 13.5);
 insert into products (name, description, code, price) values ('First Daughter', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
 
-Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', '637276459-8', 11.5); */ 
-
-insert into landen (id, name) values (1, 'Japan' );
-insert into landen (id, name) values (2, 'Italië' );
-insert into landen (id, name) values (3, 'Amerika' );
-
-/*DROP TABLE IF EXISTS relaties;
-CREATE TABLE relaties {
-  ID SERIAL PRIMARY KEY,
-  product1_id INTEGER,
-  product2_id INTEGER
-
-}*/
-
-insert into relaties (id, product1_id, product2_id) values (kfdjsakfl;dsjkfl;dsjf);
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', '637276459-8', 11.5);
+*/
